2cb250a34c738c5c0044776a9b6dc227fc12a6d0
Dalton Aaker <135642174+djaaker@users.noreply.github.com>
2024-07-11 18:18:18 -0400

    Update wave_visualizer.m

--- Rotating Waves/wave_visualizer.m
+++ Rotating Waves/wave_visualizer.m
@@ -2,7 +2,7 @@
 
 % parameters
 parameters.Fs = 1000; % data sampling rate [Hz]
-parameters.filter_order = 4; parameters.f = [5 20]; % filter parameters
+parameters.filter_order = 4; parameters.f = [40 80]; % filter parameters
 parameters.start_time = -300; % time point to start analysis
 parameters.stop_time = 0; % stop analysis (relative to target onset)
 parameters.pixel_spacing = 0.4; % spacing between electrodes [mm]
@@ -10,19 +10,31 @@ parameters.lp = 0; % cutoff for negative frequency detection [Hz]
 parameters.evaluation_angle = pi; parameters.tol = 0.2; % evaluation points
 parameters.windowBeforeCue = 1.5; % in seconds 
 parameters.windowAfterCue = 1.5; % in seconds 
-plot_rho_value = 0.7; plot_time = 20; pause_length = 0.2; 
-parameters.verbose = true; parameters.conditions = {cueHitTrace, cueMissTrace};
-parameters.extract = false;
+parameters.plot_rho_value = 0.5; plot_time = 20; pause_length = 0.2; % plot settings
+parameters.verbose = true; parameters.traces = {behaviour.cueHitTrace, behaviour.cueMissTrace};
+parameters.extract = true;
 parameters.plot = true;
 
-nTrials = numel(behaviour.cueHitTrace);
- 
+% datasets = {'Day14_BaselineWaves2.mat'};
+
+
 if parameters.extract == true 
     rotWaves = struct();
-    for i = 1:numel(parameters.conditions)
-        cond = parameters.conditions{i};
-        rotWaves.wavesHit = processWaveTrials(behaviour, cond, parameters);
-    end
+%     for ii = 1:numel(datasets)
+%         data = datasets{ii};
+%         load(data,'IntanBehaviourBaseline');
+%         behaviour = IntanBehaviourBaseline;
+        for jj = 1:numel(parameters.traces)
+            cueTrace = parameters.traces{jj};
+            if jj == 1
+                fieldName = 'wavesHit';
+            elseif jj == 2
+                fieldName = 'wavesMiss';
+            end
+            condWaves = processWaveTrials(cueTrace, parameters);
+            rotWaves.(fieldName) = condWaves;
+        end
+%     end
 end
 
 
@@ -30,12 +42,12 @@ end
 
 % end  
 
-% save('RotWavesHits_5-20_07t.mat',"rotWaves")
+save('RotWavesHits_4080_D14Cool.mat',"rotWaves")
 % load('RotWavesHits.mat','rotWaves')
 
 % rotWaves = load('RotWavesHits_5-40');
 hitsVel = []; hitsStart = []; hitsQuad = []; wavesPresAll = NaN(3001,143);
-for i = 1:numel(behaviour.cueHitTrace)
+for i = 1:3
     % wavesPresAll(:,i) = rotWaves.wavesHit(i).wavePresent;
     hitsVel = [hitsVel rotWaves.wavesHit(i).angVel];
     hitsStart = [hitsStart rotWaves.wavesHit(i).start];
@@ -62,15 +74,17 @@ if parameters.plot == true
     yfit = polyval(p, hitsStart);
     hold on;
     plot(hitsStart, yfit, '-b', 'LineWidth', 1.5);
-    hold off;
 
-    % Add average
-    avgVel = zeros(1,30);
-    for i=1:30
-        pos = find(hitsStart > (i-1)*100 + 1 & hitsStart <= i*100);
-        avgVel(i) =  mean(hitsVel(pos),'all');
-    end
-    plot(avgVel)
+
+%     % Add average
+%     avgVel = zeros(1,30);
+%     for i=1:30
+%         pos = find(hitsStart > (i-1)*100 + 1 & hitsStart <= i*100);
+%         avgVel(i) =  mean(hitsVel(pos),'all');
+%     end
+%     hold on;
+%     plot(avgVel)
+%     hold off;
     
     % Add a red vertical line in the center
     xCenter = (max(hitsStart) + min(hitsStart)) / 2;
@@ -78,18 +92,18 @@ if parameters.plot == true
     
     % Distribution of waves in hit trials
     subplot(2, 1, 2);
-    histogram(hitsQuad, 'BinLimits', [0 16], 'BinWidth', 1);
+    histogram(hitsQuad, 'BinLimits', [1 17], 'BinWidth', 1);
     hold on;
-    xlim([0 16]);
+    xlim([0 17]);
     
     % Fit a normal distribution to the data
     pd = fitdist(hitsQuad', 'Normal');
-    xValues = linspace(0, 16, 100); % X values from 0 to 16
+    xValues = linspace(0, 17, 100); % X values from 0 to 16
     yValues = pdf(pd, xValues) * numel(hitsQuad) * (xValues(2) - xValues(1)); % Scaled PDF
     
     plot(xValues, yValues, 'r-', 'LineWidth', 1.5); % Plot the trend line within the limited range
     
-    line([8 8], ylim, 'Color', 'r', 'LineStyle', '--'); % Red vertical line at x = 8
+    line([9 9], ylim, 'Color', 'r', 'LineStyle', '--'); % Red vertical line at x = 8
     
     hold off;
     xlabel('Quadrants');
